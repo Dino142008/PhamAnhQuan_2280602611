c4fc6ce846f1e1a0c43600b8e6c1b1e3ec2dcd72
MSI KATANA <MSI KATANA@DESKTOP-F2T5RUN>
2024-12-25 10:29:36 +0700

    Add project files.

---
+++ PhamAnhQuan_2280602611/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace PhamAnhQuan_2280602611
+{
+    partial class frmSanpham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btntim = new System.Windows.Forms.Button();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnkluu = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.dtNgaynhap = new System.Windows.Forms.DateTimePicker();
+            this.dtgSanpham = new System.Windows.Forms.DataGridView();
+            this.cmbLoaiSP = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSanpham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DANH MỤC SẢN PHẨM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã SP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên SP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(565, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ngày Nhập";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(565, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Loại SP";
+            // 
+            // btntim
+            // 
+            this.btntim.Location = new System.Drawing.Point(238, 77);
+            this.btntim.Name = "btntim";
+            this.btntim.Size = new System.Drawing.Size(75, 23);
+            this.btntim.TabIndex = 6;
+            this.btntim.Text = "Tìm";
+            this.btntim.UseVisualStyleBackColor = true;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(37, 345);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 7;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(137, 344);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 8;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(250, 344);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 9;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(358, 344);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 23);
+            this.btnluu.TabIndex = 10;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            // 
+            // btnkluu
+            // 
+            this.btnkluu.Location = new System.Drawing.Point(474, 344);
+            this.btnkluu.Name = "btnkluu";
+            this.btnkluu.Size = new System.Drawing.Size(75, 23);
+            this.btnkluu.TabIndex = 11;
+            this.btnkluu.Text = "K.Lưu";
+            this.btnkluu.UseVisualStyleBackColor = true;
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(579, 344);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 12;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Location = new System.Drawing.Point(661, 126);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(100, 22);
+            this.txtMaSP.TabIndex = 13;
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Location = new System.Drawing.Point(661, 178);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(100, 22);
+            this.txtTenSP.TabIndex = 14;
+            // 
+            // dtNgaynhap
+            // 
+            this.dtNgaynhap.Location = new System.Drawing.Point(661, 226);
+            this.dtNgaynhap.Name = "dtNgaynhap";
+            this.dtNgaynhap.Size = new System.Drawing.Size(200, 22);
+            this.dtNgaynhap.TabIndex = 15;
+            // 
+            // dtgSanpham
+            // 
+            this.dtgSanpham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgSanpham.Location = new System.Drawing.Point(12, 123);
+            this.dtgSanpham.Name = "dtgSanpham";
+            this.dtgSanpham.RowHeadersWidth = 51;
+            this.dtgSanpham.RowTemplate.Height = 24;
+            this.dtgSanpham.Size = new System.Drawing.Size(502, 189);
+            this.dtgSanpham.TabIndex = 17;
+            // 
+            // cmbLoaiSP
+            // 
+            this.cmbLoaiSP.FormattingEnabled = true;
+            this.cmbLoaiSP.Location = new System.Drawing.Point(661, 277);
+            this.cmbLoaiSP.Name = "cmbLoaiSP";
+            this.cmbLoaiSP.Size = new System.Drawing.Size(121, 24);
+            this.cmbLoaiSP.TabIndex = 18;
+            // 
+            // frmSanpham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 494);
+            this.Controls.Add(this.cmbLoaiSP);
+            this.Controls.Add(this.dtgSanpham);
+            this.Controls.Add(this.dtNgaynhap);
+            this.Controls.Add(this.txtTenSP);
+            this.Controls.Add(this.txtMaSP);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnkluu);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.btntim);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmSanpham";
+            this.Text = "QUAN LY SAN PHAM";
+            this.Load += new System.EventHandler(this.frmSanpham_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSanpham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btntim;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Button btnkluu;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.DateTimePicker dtNgaynhap;
+        private System.Windows.Forms.DataGridView dtgSanpham;
+        private System.Windows.Forms.ComboBox cmbLoaiSP;
+    }
+}
+
---
+++ PhamAnhQuan_2280602611/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+using PhamAnhQuan_2280602611.model;
+
+namespace PhamAnhQuan_2280602611
+{
+    public partial class frmSanpham : Form
+    {
+        public frmSanpham()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSanpham_Load(object sender, EventArgs e)
+        {
+            LoadLoaiSP();
+            LoadSanpham();
+        }
+
+        private void LoadLoaiSP()
+        {
+            using (var dbContext = new Model1())
+            {
+                var loaiSPList = dbContext.LoaiSPs.ToList();
+                cmbLoaiSP.DataSource = loaiSPList;
+                cmbLoaiSP.DisplayMember = "TenLoai"; // Tên hiển thị
+                cmbLoaiSP.ValueMember = "MaLoai"; // Giá trị được lưu
+            }
+        }
+
+        private void LoadSanpham()
+        {
+            using (var dbContext = new Model1())
+            {
+                var sanphamList = dbContext.Sanphams.Include(s => s.LoaiSP).ToList(); // Bao gồm thông tin loại sản phẩm nếu cần
+                dtgSanpham.DataSource = sanphamList.Select(sp => new
+                {
+                    sp.MaSP,
+                    sp.TenSP,
+                    Ngaynhap = sp.Ngaynhap.ToString("yyyy-MM-dd"),
+                    sp.MaLoai,
+                    TenLoai = sp.LoaiSP.TenLoai // Lấy tên loại sản phẩm
+                }).ToList();
+            }
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Nút đã được nhấn!"); // Thông báo kiểm tra
+
+                using (var dbContext = new Model1())
+                {
+                    // Lấy thông tin sản phẩm từ các trường nhập liệu
+                    string maSP = txtMaSP.Text;
+                    string tenSP = txtTenSP.Text;
+                    DateTime ngaynhap = dtNgaynhap.Value;
+                    string maLoai = cmbLoaiSP.SelectedValue.ToString();
+
+                    // Tạo đối tượng sản phẩm mới
+                    Sanpham newSanpham = new Sanpham
+                    {
+                        MaSP = maSP,
+                        TenSP = tenSP,
+                        Ngaynhap = ngaynhap,
+                        MaLoai = maLoai
+                    };
+
+                    // Thêm sản phẩm vào DbSet và lưu thay đổi
+                    dbContext.Sanphams.Add(newSanpham);
+                    dbContext.SaveChanges();
+
+                    // Cập nhật DataGridView
+                    LoadSanpham();
+
+                    MessageBox.Show("Thêm sản phẩm thành công!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message);
+            }
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close(); // Đóng form
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
